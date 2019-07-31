a3ad11bb75b31f82af4208137a2c18808513bb8c
Harvey Kelly <harveyjk86@gmail.com>
2019-07-31 09:26:32 +0000

    Added uglify functionality to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,31 @@ module.exports = function(grunt) {
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+      uglify: {
+        main: {
+          options: {
+            mangle: false,
+            sourceMap: {
+              includeSources: true
+            },
+            compress: {
+              drop_console: true
+            },
+            banner: '<%= banner %>'
+          },
+          files: [{
+            expand: true,
+            cwd: 'src',
+            src: '**/*.js',
+            dest: 'dist'
+          }]
+        }
+      }
   });
 
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify:main']);
 
 };
